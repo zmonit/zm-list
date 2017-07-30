8be206dcaed53cdc9a8e221f7b62bd75cf667927
Michal Vyskocil <michal.vyskocil@gmail.com>
2017-07-30 14:23:20 +0200

    Problem: zm-list isn't nore than skeleton
    Solution: drop data class, call is zm_cache. Write a basic unit test, comment a
    problem of API and make the proect to pass make, check and memcheck

--- src/zm_list.c
+++ src/zm_list.c
@@ -1,5 +1,5 @@
 /*  =========================================================================
-    zm_list - Main daemon
+    zm_list - zm list actor
 
     Copyright (c) the Contributors as noted in the AUTHORS file.  This file is part
     of zmon.it, the fast and scalable monitoring system.                           
@@ -12,36 +12,173 @@
 
 /*
 @header
-    zm_list - Main daemon
+    zm_list - zm list actor
 @discuss
 @end
 */
 
-#include "zm_device_classes.h"
+#include "zm_list_classes.h"
 
-int main (int argc, char *argv [])
+//  Structure of our actor
+
+struct _zm_list_t {
+    zsock_t *pipe;              //  Actor command pipe
+    zpoller_t *poller;          //  Socket poller
+    bool terminated;            //  Did caller ask us to quit?
+    bool verbose;               //  Verbose logging enabled?
+    //  TODO: Declare properties
+};
+
+
+//  --------------------------------------------------------------------------
+//  Create a new zm_list instance
+
+static zm_list_t *
+zm_list_new (zsock_t *pipe, void *args)
 {
-    bool verbose = false;
-    int argn;
-    for (argn = 1; argn < argc; argn++) {
-        if (streq (argv [argn], "--help")
-        ||  streq (argv [argn], "-h")) {
-            puts ("zm-list [options] ...");
-            puts ("  --verbose / -v         verbose test output");
-            puts ("  --help / -h            this information");
-            return 0;
-        }
-        else
-        if (streq (argv [argn], "--verbose")
-        ||  streq (argv [argn], "-v"))
-            verbose = true;
-        else {
-            printf ("Unknown option: %s\n", argv [argn]);
-            return 1;
-        }
+    zm_list_t *self = (zm_list_t *) zmalloc (sizeof (zm_list_t));
+    assert (self);
+
+    self->pipe = pipe;
+    self->terminated = false;
+    self->poller = zpoller_new (self->pipe, NULL);
+
+    //  TODO: Initialize properties
+
+    return self;
+}
+
+
+//  --------------------------------------------------------------------------
+//  Destroy the zm_list instance
+
+static void
+zm_list_destroy (zm_list_t **self_p)
+{
+    assert (self_p);
+    if (*self_p) {
+        zm_list_t *self = *self_p;
+
+        //  TODO: Free actor properties
+
+        //  Free object itself
+        zpoller_destroy (&self->poller);
+        free (self);
+        *self_p = NULL;
     }
-    //  Insert main code here
-    if (verbose)
-        zsys_info ("zm_list - Main daemon");
+}
+
+
+//  Start this actor. Return a value greater or equal to zero if initialization
+//  was successful. Otherwise -1.
+
+static int
+zm_list_start (zm_list_t *self)
+{
+    assert (self);
+
+    //  TODO: Add startup actions
+
     return 0;
+}
+
+
+//  Stop this actor. Return a value greater or equal to zero if stopping 
+//  was successful. Otherwise -1.
+
+static int
+zm_list_stop (zm_list_t *self)
+{
+    assert (self);
+
+    //  TODO: Add shutdown actions
+
+    return 0;
+}
+
+
+//  Here we handle incoming message from the node
+
+static void
+zm_list_recv_api (zm_list_t *self)
+{
+    //  Get the whole message of the pipe in one go
+    zmsg_t *request = zmsg_recv (self->pipe);
+    if (!request)
+       return;        //  Interrupted
+
+    char *command = zmsg_popstr (request);
+    if (streq (command, "START"))
+        zm_list_start (self);
+    else
+    if (streq (command, "STOP"))
+        zm_list_stop (self);
+    else
+    if (streq (command, "VERBOSE"))
+        self->verbose = true;
+    else
+    if (streq (command, "$TERM"))
+        //  The $TERM command is send by zactor_destroy() method
+        self->terminated = true;
+    else {
+        zsys_error ("invalid command '%s'", command);
+        assert (false);
+    }
+    zstr_free (&command);
+    zmsg_destroy (&request);
+}
+
+
+//  --------------------------------------------------------------------------
+//  This is the actor which runs in its own thread.
+
+void
+zm_list_actor (zsock_t *pipe, void *args)
+{
+    zm_list_t * self = zm_list_new (pipe, args);
+    if (!self)
+        return;          //  Interrupted
+
+    //  Signal actor successfully initiated
+    zsock_signal (self->pipe, 0);
+
+    while (!self->terminated) {
+        zsock_t *which = (zsock_t *) zpoller_wait (self->poller, 0);
+        if (which == self->pipe)
+            zm_list_recv_api (self);
+       //  Add other sockets when you need them.
+    }
+    zm_list_destroy (&self);
+}
+
+//  --------------------------------------------------------------------------
+//  Self test of this actor.
+
+void
+zm_list_test (bool verbose)
+{
+    printf (" * zm_list: ");
+    //  @selftest
+    //  Simple create/destroy test
+    // Note: If your selftest reads SCMed fixture data, please keep it in
+    // src/selftest-ro; if your test creates filesystem objects, please
+    // do so under src/selftest-rw. They are defined below along with a
+    // usecase for the variables (assert) to make compilers happy.
+    const char *SELFTEST_DIR_RO = "src/selftest-ro";
+    const char *SELFTEST_DIR_RW = "src/selftest-rw";
+    assert (SELFTEST_DIR_RO);
+    assert (SELFTEST_DIR_RW);
+    // Uncomment these to use C++ strings in C++ selftest code:
+    //std::string str_SELFTEST_DIR_RO = std::string(SELFTEST_DIR_RO);
+    //std::string str_SELFTEST_DIR_RW = std::string(SELFTEST_DIR_RW);
+    //assert ( (str_SELFTEST_DIR_RO != "") );
+    //assert ( (str_SELFTEST_DIR_RW != "") );
+    // NOTE that for "char*" context you need (str_SELFTEST_DIR_RO + "/myfilename").c_str()
+
+    zactor_t *zm_list = zactor_new (zm_list_actor, NULL);
+
+    zactor_destroy (&zm_list);
+    //  @end
+
+    printf ("OK\n");
 }
